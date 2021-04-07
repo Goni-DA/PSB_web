b42955dea6aa22c9b29e4900361dd059712a7648
wnsdud412 <wnsdud412@naver.com>
2021-04-07 12:19:17 +0900

    x, y좌표가 주어진다면? 최종 model import 완료

--- PSB.py
+++ PSB.py
@@ -2,12 +2,15 @@ from flask import Flask, render_template, redirect, url_for, request, jsonify
 from xgboost import XGBRegressor
 import numpy as np
 import pandas as pd
+import sklearn
 import json
 import pickle
+import PSB_manage as pm
 
 
 app = Flask(__name__)
-model = pickle.load(open('psb.pkl', 'rb'))
+model = pickle.load(open('data.pkl', 'rb'))
+cols_check = model.get_booster().feature_names
 
 @app.route("/")
 def test1():
@@ -15,22 +18,30 @@ def test1():
 
 @app.route('/predict', methods=['POST'])
 def home():
-    data1 = float(request.form['a'])
-    data2 = int(request.form['b'])
-    data3 = int(request.form['c'])
+    data1 = int(request.form['a'])
+    #data2 = int(request.form['b']) #safe_center
+    #data3 = float(request.form['c']) #onspot
     data4 = int(request.form['d'])
     data5 = int(request.form['e'])
     data6 = int(request.form['f'])
     data7 = int(request.form['g'])
+    data8 = float(request.form['h'])
+    data9 = float(request.form['i'])
 
-    df = pd.DataFrame(columns=['ONSPOT_DSTN', 'GOUT_FIRESTTN_NM', 'GOUT_SAFE_CENTER_NM', 'STATMNT_TM',
-       'EMRLF_EMD_NM', 'JURISD_DIV_NM_CNT', 'JURISD_DIV_NM_OUT'])
-    df = df.append({'ONSPOT_DSTN': data1, 'GOUT_FIRESTTN_NM': data2, 'GOUT_SAFE_CENTER_NM': data3, 'STATMNT_TM': data4,
-                    'EMRLF_EMD_NM': data5, 'JURISD_DIV_NM_CNT': data6, 'JURISD_DIV_NM_OUT': data7}, ignore_index=True)
+    data2 = pm.dist(data8, data9)
+    data3 = pm.dist_only(data8, data9)
+
+
+    df = pd.DataFrame(columns=['GOUT_FIRESTTN_NM', 'GOUT_SAFE_CENTER_NM', 'ONSPOT_DSTN',
+       'EMRLF_EMD_NM', 'RELIF_OCCURPLC_TYPE', 'STATMNT_TM','JURISD_DIV_NM_CNT'])
+    df = df.append({'GOUT_FIRESTTN_NM': data1, 'GOUT_SAFE_CENTER_NM': data2, 'ONSPOT_DSTN': data3, 'EMRLF_EMD_NM': data4,
+                    'RELIF_OCCURPLC_TYPE': data5, 'STATMNT_TM': data6, 'JURISD_DIV_NM_CNT': data7}, ignore_index=True)
     df2 = df.astype(np.int64)
     df3 = df2.ONSPOT_DSTN.astype(np.float64)
     df2 = df2.drop(columns='ONSPOT_DSTN')
     df4 = pd.concat((df3,df2), axis=1)
+    df4 = df4[cols_check]
+
     pred = model.predict(df4)
     return render_template('after.html', data=pred)
 
--- PSB_manage.py
+++ PSB_manage.py
@@ -1,69 +1,110 @@
 import cx_Oracle as oci
+from haversine import haversine
 
 oracle_dsn = oci.makedsn(host="192.168.2.247", port=1521, sid="orcl")
 conn = oci.connect(dsn=oracle_dsn, user="emg", password="1234566")
 
-class aid:
-    def __init__(self,
-                 onspot_dstn, gout_firesttn_nm, gout_safe_center_nm, statmnt_tm,emrlf_emd_nm, jurisd_div_nm_cnt, jurisd_div_nm_out
-                 salary=None, commission_pct=None,
-                 manager_id=None, department_id=None):
-        self.ONSPOT_DSTN = ONSPOT_DSTN
-        self.GOUT_FIRESTTN_NM = GOUT_FIRESTTN_NM
-        self.GOUT_SAFE_CENTER_NM = GOUT_SAFE_CENTER_NM
-        self.STATMNT_TM = STATMNT_TM
-        self.EMRLF_EMD_NM = EMRLF_EMD_NM
-        self.JURISD_DIV_NM_CNT = JURISD_DIV_NM_CNT
-        self.JURISD_DIV_NM_OUT = JURISD_DIV_NM_OUT
-        self.salary = salary
-        self.commission_pct = commission_pct
-        self.manager_id = manager_id
-        self.department_id = department_id
-
-    def __str__(self):
-        return f"{self.ONSPOT_DSTN}, {self.GOUT_FIRESTTN_NM}, {self.GOUT_SAFE_CENTER_NM}, " \
-               f"{self.STATMNT_TM}, {self.phone_number}, {self.hire_date}, " \
-               f"{self.job_id}, {self.salary}, {self.commission_pct}, " \
-               f"{self.manager_id}, {self.department_id}"
-
-    def to_dict(self):
-        return {"employee_id": self.employee_id,
-                "first_name": self.first_name,
-                "last_name": self.last_name,
-                "email": self.email,
-                "phone_number": self.phone_number,
-                "hire_date": self.hire_date,
-                "job_id": self.job_id,
-                "salary": self.salary,
-                "commission_pct": self.commission_pct,
-                "manager_id": self.manager_id,
-                "department_id": self.department_id}
+# class aid:
+#     def __init__(self,
+#                  onspot_dstn, gout_firesttn_nm, gout_safe_center_nm, statmnt_tm,emrlf_emd_nm, jurisd_div_nm_cnt, jurisd_div_nm_out
+#                  salary=None, commission_pct=None,
+#                  manager_id=None, department_id=None):
+#         self.ONSPOT_DSTN = ONSPOT_DSTN
+#         self.GOUT_FIRESTTN_NM = GOUT_FIRESTTN_NM
+#         self.GOUT_SAFE_CENTER_NM = GOUT_SAFE_CENTER_NM
+#         self.STATMNT_TM = STATMNT_TM
+#         self.EMRLF_EMD_NM = EMRLF_EMD_NM
+#         self.JURISD_DIV_NM_CNT = JURISD_DIV_NM_CNT
+#         self.JURISD_DIV_NM_OUT = JURISD_DIV_NM_OUT
+#         self.salary = salary
+#         self.commission_pct = commission_pct
+#         self.manager_id = manager_id
+#         self.department_id = department_id
+#
+#     def __str__(self):
+#         return f"{self.ONSPOT_DSTN}, {self.GOUT_FIRESTTN_NM}, {self.GOUT_SAFE_CENTER_NM}, " \
+#                f"{self.STATMNT_TM}, {self.phone_number}, {self.hire_date}, " \
+#                f"{self.job_id}, {self.salary}, {self.commission_pct}, " \
+#                f"{self.manager_id}, {self.department_id}"
+#
+#     def to_dict(self):
+#         return {"employee_id": self.employee_id,
+#                 "first_name": self.first_name,
+#                 "last_name": self.last_name,
+#                 "email": self.email,
+#                 "phone_number": self.phone_number,
+#                 "hire_date": self.hire_date,
+#                 "job_id": self.job_id,
+#                 "salary": self.salary,
+#                 "commission_pct": self.commission_pct,
+#                 "manager_id": self.manager_id,
+#                 "department_id": self.department_id}
 
 
 def get_geoinfo():
-    sql = "select * from lacation"
+    sql = "select * from locations"
     cursor = conn.cursor()
     cursor.execute(sql)
     geoinfo = cursor.fetchall()
     return geoinfo
 
+x = 37.643217
+y = 126.788109
+#sample = (37.643217, 126.788109) #백석역, 받아온 좌표값으로 대체할 것
 
-sample = (37.643217, 126.788109) #백석역, 받아온 좌표값으로 대체할 것
-def dist(sample):
+def dist(x, y):
+    cursor = conn.cursor()
+
+    sql = 'select * from locations'
+    cursor.execute(sql)
+
     lst = []
     for i in cursor:
         center_119 = (float(i[2]), float(i[1]))
-        temp = haversine(sample, center_119, unit='km')
+        temp = haversine((x, y), center_119, unit='km')
         lst.append((temp, i[0], i[3]))
     lst.sort()
     return (lst[0][1],lst[0][2])
-a = dist(sample)
-print(a)
+
+def get_safe(x, y):
+    cursor = conn.cursor()
+
+    sql = 'select * from locations'
+    cursor.execute(sql)
+
+    lst = []
+    for i in cursor:
+        center_119 = (float(i[2]), float(i[1]))
+        temp = haversine((x, y), center_119, unit='km')
+        lst.append((temp, i[0], i[3]))
+    lst.sort()
+    return lst[0][2]
+
+def dist_only(x, y):
+    cursor = conn.cursor()
+
+    sql = 'select * from locations'
+    cursor.execute(sql)
+
+    lst = []
+    for i in cursor:
+        center_119 = (float(i[2]), float(i[1]))
+        temp = haversine((x, y), center_119, unit='km')
+        lst.append((temp))
+    lst.sort()
+    return (lst[0])
+
+a = dist(x, y)
+b = get_safe(x, y)
+c = dist_only(x, y)
+print("dist :" + str(a))
+print("safe_label : " + str(b))
+print("dist_only : " + str(c))
 
 def get_donginfo(dong_nm):
     sql = "select LABEL from DONG where DONG =:dong_nm"
     cursor = conn.cursor()
-    cursor.execute(sql, {'dong_nm':dong_nm})
+    cursor.execute(sql, {'dong_nm': dong_nm})
     donginfo = cursor.fetchone()
     dong2 = donginfo[0]
     return dong2
@@ -76,5 +117,5 @@ def get_safeinfo(safe_id):
     return safeinfo
 
 dong_nm = '연무동'
-a = get_donginfo(dong_nm)
-print(a)
+b = get_donginfo(dong_nm)
+print(b)
